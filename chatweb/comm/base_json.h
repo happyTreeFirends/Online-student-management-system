b4813e4ffbfad429f3181b56d4d5604f896431ed
happyTreeFirends <294636185@qq.com>
2018-09-09 18:23:24 -0700

    delete error

--- chatweb/comm/base_json.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include<iostream>
-#include<json/json.h>
-
-void stringToValue(std::string &inString,Json::Value &root);
-void valueToString(Json::Value &root,std::string &outString);
